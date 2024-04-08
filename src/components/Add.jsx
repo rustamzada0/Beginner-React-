0de4794bfd91799b7882b528b614cefcbc56c9a6
rustamzada0 <rustemzade.tunay.2000@gmail.com>
2024-04-08 16:07:40 +0400

    React-Pose-Animation

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
+import posed from 'react-pose';
+
+
+const Animation = posed.div({
+  visible: { opacity: 1, applyAtStart: { display: 'block' } },
+  hidden: { opacity: 0, applyAtEnd: { display: 'none' } }
+})
 
 class Add extends Component {
+
+  state = {
+    isVisible: true
+  }
+
+  changeVisibility = (e) => {
+    this.setState({ isVisible: !this.state.isVisible })
+  }
+
   render() {
+
+    const { isVisible } = this.state
+
     return (
       <div className='col-md-8 mb-4'>
+      <button onClick={this.changeVisibility} className='btn btn-dark btn-block mb-4'>{isVisible ? 'Hide Form' : 'Show Form'}</button>
+      <Animation pose={isVisible ? 'visible' : 'hidden'}>
         <div className="card">
             <div className="card-header">
                 <h4>Add User Form</h4>
@@ -28,6 +49,7 @@ class Add extends Component {
                 </form>
             </div>
         </div>
+      </Animation>
       </div>
     )
   }
